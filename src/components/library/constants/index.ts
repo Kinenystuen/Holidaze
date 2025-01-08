420ba45d7ed6801fb03f52dbe5ac30332a474a48
Kinenystuen <Kine_on1@hotmail.com>
2025-01-08 11:30:37 +0100

    constants

---
+++ src/components/library/constants/index.ts
@@ -0,0 +1,14 @@
+export const apiKey = import.meta.env.VITE_API_HOST_URL;
+
+export const headerNavLinks = [
+  {
+    title: "Home",
+    route: "/",
+    label: "Home"
+  },
+  {
+    title: "Venues",
+    route: "/venues",
+    label: "Venues"
+  }
+];
