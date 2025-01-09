e8d60cfcc52837897dede136fd9146a068657d16
Kinenystuen <Kine_on1@hotmail.com>
2025-01-09 14:33:47 +0100

    Hero homepage

--- src/components/HeroHome.tsx
+++ src/components/HeroHome.tsx
@@ -1,3 +1,4 @@
+import SearchBarHome from "../_root/pages/home/SearchBarHome";
 import H1 from "./shared/Typography/H1";
 import P from "./shared/Typography/P";
 
@@ -5,9 +6,12 @@ const HeroHome = () => {
   return (
     <div className="relative z-10">
       <div className="container mx-auto flex flex-col items-center justify-center my-6 md:my-10 2xl:my-20 px-4">
-        <div className="text-center max-w-lg">
+        <div className="relative text-center max-w-lg">
           <H1 className="font-heading text-4xl md:text-6xl font-bold text-balance leading-tight text-gray-800">
-            Book Your Next Adventure
+            Book Your Next
+            <span className="text-color3-600 dark:text-color2-500">
+              Adventure
+            </span>
           </H1>
           <P className="mt-2 text-lg md:text-xl text-gray-600 text-balance">
             Explore unique destinations, create unforgettable memories, and
@@ -15,21 +19,7 @@ const HeroHome = () => {
           </P>
 
           {/* Search Bar */}
-          <div className="mt-8 w-full flex justify-center">
-            <div className="relative w-full max-w-md flex">
-              <input
-                type="text"
-                placeholder="Search for venues..."
-                className="w-full px-4 py-3 text-lg rounded-s-full border border-gray-300 focus:ring-2 focus:ring-color2 focus:outline-none"
-              />
-              <button
-                type="button"
-                className="px-6 py-3 bg-color1 text-white font-semibold rounded-r-full hover:bg-color3 transition duration-300"
-              >
-                Search
-              </button>
-            </div>
-          </div>
+          <SearchBarHome />
         </div>
       </div>
     </div>
