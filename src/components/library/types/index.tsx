127c4d972cf29694c61547f9eb2aea421a31d4ea
Kinenystuen <Kine_on1@hotmail.com>
2025-01-09 14:39:35 +0100

    Types updated

--- src/components/library/types/index.tsx
+++ src/components/library/types/index.tsx
@@ -30,12 +30,77 @@ export interface LoaderProps {
   className?: string;
 }
 
-/* Venue props */
+/* Venue props */ /////////////////////////////
+export interface Media {
+  url: string;
+  alt: string;
+}
+
+export interface Meta {
+  wifi: boolean;
+  parking: boolean;
+  breakfast: boolean;
+  pets: boolean;
+}
+
+export interface Location {
+  address: string;
+  city: string;
+  zip: string;
+  country: string;
+  continent: string;
+  lat: number;
+  lng: number;
+}
+
+export interface Avatar {
+  url: string;
+  alt: string;
+}
+
+export interface Banner {
+  url: string;
+  alt: string;
+}
+
+export interface Owner {
+  name: string;
+  email: string;
+  bio: string;
+  avatar: Avatar;
+  banner: Banner;
+}
+
+export interface Customer {
+  name: string;
+  email: string;
+  bio: string;
+  avatar: Avatar;
+  banner: Banner;
+}
+
+export interface Booking {
+  id: string;
+  dateFrom: string;
+  dateTo: string;
+  guests: number;
+  created: string;
+  updated: string;
+  customer: Customer;
+}
+
 export interface Venue {
   id: string;
   name: string;
   description: string;
-  media: { url: string; alt: string }[];
+  media: Media[];
   price: number;
+  maxGuests: number;
   rating: number;
+  created: string;
+  updated: string;
+  meta: Meta;
+  location: Location;
+  owner: Owner;
+  bookings: Booking[];
 }
