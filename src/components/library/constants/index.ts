33986465f2612a69755249d3a90c6d1cf7a0d868
Kinenystuen <Kine_on1@hotmail.com>
2025-01-09 10:06:47 +0100

    apiHostUrl added

--- src/components/library/constants/index.ts
+++ src/components/library/constants/index.ts
@@ -1,4 +1,5 @@
-export const apiKey = import.meta.env.VITE_API_HOST_URL;
+export const apiHostUrl = import.meta.env.VITE_API_HOST_URL;
+export const apiKey = import.meta.env.VITE_API_KEY;
 
 export const headerNavLinks = [
   {
