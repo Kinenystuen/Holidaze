f0de75704ab767568c69ce89f3eb0dfd50b55182
Kinenystuen <Kine_on1@hotmail.com>
2025-01-09 08:19:16 +0100

    Routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import RootLayout from "./_root/RootLayout";
-import Home from "./_root/pages/Home";
+import Home from "./_root/pages/home/Home";
+import Venues from "./components/venues/venues";
 
 function App() {
   return (
     <>
       <Routes>
+        {/* Auth routes */}
+        <Route
+          path="auth"
+          element={
+            <div className="bg-slate-700 text-white min-h-screen flex items-center justify-center">
+              Auth
+              <Link to="/">Home</Link>
+            </div>
+          }
+        />
         {/* Public routes */}
         <Route element={<RootLayout />}>
-          <Route index element={<Home></Home>} />
-          {/* <Route path="gallery" element={<PageGallery />} />
-          <Route path="gallery/:id" element={<PageGallery />} /> */}
+          <Route index element={<Home />} />
+          <Route path="venues" element={<Venues />} />
+          {/* <Route path="gallery/:id" element={<PageGallery />} />  */}
           <Route path="*" element={<div>Page not found</div>} />
         </Route>
       </Routes>
