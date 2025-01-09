115daef48d528db8b7a18cc5c3500f7b6e199b99
Kinenystuen <Kine_on1@hotmail.com>
2025-01-09 14:13:10 +0100

    Errorpage linked

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Link, Route, Routes } from "react-router-dom";
 import RootLayout from "./_root/RootLayout";
 import Home from "./_root/pages/home/Home";
 import Venues from "./components/venues/venues";
+import ErrorMessage from "./components/shared/ErrorMessage";
+import Button from "./components/shared/Button/Button";
 
 function App() {
   return (
@@ -22,7 +24,16 @@ function App() {
           <Route index element={<Home />} />
           <Route path="venues" element={<Venues />} />
           {/* <Route path="gallery/:id" element={<PageGallery />} />  */}
-          <Route path="*" element={<div>Page not found</div>} />
+          <Route
+            path="*"
+            element={
+              <ErrorMessage message="Page not found">
+                <Link to="/">
+                  <Button buttonType="violet">Go Back</Button>
+                </Link>
+              </ErrorMessage>
+            }
+          />
         </Route>
       </Routes>
     </>
