9206500f619cc175a71179687c7a602f93f4d9ac
Kinenystuen <Kine_on1@hotmail.com>
2025-01-09 08:30:21 +0100

    Props added

--- src/components/library/types/index.tsx
+++ src/components/library/types/index.tsx
@@ -3,3 +3,29 @@ export type INavLink = {
   route: string;
   label: string;
 };
+
+/* Button props */
+export type ButtonProps = {
+  ButtonType?: "primary" | "secondary";
+  onClick?: () => void;
+  children: React.ReactNode;
+  className?: string;
+  title?: string;
+  ariaLabel?: string;
+  disabled?: boolean;
+  buttonType?: "violet" | "blue" | "transparent";
+  type?: "button" | "submit" | "reset";
+};
+
+/* Typographic props */
+export type H1Props = {
+  className?: string;
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+};
+
+/* Loader Props */
+export interface LoaderProps {
+  theme?: "light" | "dark";
+  className?: string;
+}
