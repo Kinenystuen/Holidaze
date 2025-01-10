bd4a5031f8c5909762ef4a6162e2256e730d3ec7
Kinenystuen <Kine_on1@hotmail.com>
2025-01-10 16:24:48 +0100

    Updated error message props

--- src/components/shared/ErrorMessage.tsx
+++ src/components/shared/ErrorMessage.tsx
@@ -5,25 +5,47 @@ import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 import H2 from "./Typography/H2";
 
 interface ErrorMessageProps {
-  message: string;
+  message?: string;
   children?: React.ReactNode;
   name?: string;
+  className?: string;
+  fontAwesomeIcon?: React.ReactNode;
+  icon?: boolean;
 }
 
 const ErrorMessage: React.FC<ErrorMessageProps> = ({
   message,
   children,
-  name
+  name,
+  className = "",
+  fontAwesomeIcon,
+  icon = true
 }) => {
   return (
-    <section className="container w-full mb-10 flex justify-center items-center flex-col min-h-[60vh]">
-      <div className="my-10 text-center">
-        <FontAwesomeIcon icon={faExclamationCircle} className="text-5xl m-3" />
+    <section
+      role="alert"
+      aria-label={`Error: ${name || "Data Fetching"}`}
+      className={`container w-full mb-10 flex justify-center items-center flex-col min-h-[60vh] ${className}`}
+    >
+      <div className="text-center">
+        {icon && (
+          <div>
+            {fontAwesomeIcon ? (
+              fontAwesomeIcon
+            ) : (
+              <FontAwesomeIcon
+                icon={faExclamationCircle}
+                className="text-color2-900 text-5xl p-4"
+              />
+            )}
+          </div>
+        )}
         {name && (
           <H2 className="uppercase font-bold">Error fetching {name} data</H2>
         )}
         <P className="uppercase font-bold">{message}</P>
       </div>
+
       {children}
     </section>
   );
