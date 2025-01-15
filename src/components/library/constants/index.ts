215adbcaafeb7464045015246a681153c9bcd2e2
Kinenystuen <Kine_on1@hotmail.com>
2025-01-15 19:46:53 +0100

    Removed home from header nav

--- src/components/library/constants/index.ts
+++ src/components/library/constants/index.ts
@@ -1,15 +1,21 @@
+import { User } from "../types";
+
 export const apiHostUrl = import.meta.env.VITE_API_HOST_URL;
 export const apiKey = import.meta.env.VITE_API_KEY;
 
 export const headerNavLinks = [
-  {
-    title: "Home",
-    route: "/",
-    label: "Home"
-  },
   {
     title: "Venues",
     route: "/venues",
     label: "Venues"
   }
 ];
+
+export const INITIAL_USER: User = {
+  // id: "",
+  name: "",
+  email: "",
+  avatarUrl: "",
+  bannerUrl: "",
+  venueManager: false
+};
