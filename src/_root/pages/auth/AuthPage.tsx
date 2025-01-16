3ba95b0e128b9f457fdb31961257ee7889ba3030
Kinenystuen <Kine_on1@hotmail.com>
2025-01-16 09:38:55 +0100

    Auth page layout change

--- src/_root/pages/auth/AuthPage.tsx
+++ src/_root/pages/auth/AuthPage.tsx
@@ -2,17 +2,10 @@ import React from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import LoginForm from "../../../components/Auth/LoginForm";
 import RegisterForm from "../../../components/Auth/RegisterForm";
-import Button from "../../../components/shared/Button/Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons/faArrowAltCircleLeft";
 
 const AuthPage: React.FC = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const type = searchParams.get("type") || "login"; // Default to "login"
-
-  const handleTypeChange = (newType: string) => {
-    setSearchParams({ type: newType });
-  };
+  const [searchParams] = useSearchParams();
+  const type = searchParams.get("type") || "login";
 
   return (
     <div className="auth-container h-screen w-screen bg-slate-800 dark:bg-gray-900 relative">
@@ -23,49 +16,38 @@ const AuthPage: React.FC = () => {
           alt="Plane in the sky"
           className="object-cover h-full w-full"
         />
-        <div className="absolute inset-0 bg-black bg-opacity-30 dark:bg-opacity-40 z-10"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-50 dark:bg-opacity-50 z-10"></div>
       </div>
 
       {/* Content */}
       <div className="relative z-20 max-w-md mx-auto h-[90vh] flex flex-col justify-center items-center ">
-        {/* Tab Buttons */}
-        <div className="tab-buttons flex justify-center items-center w-full ">
-          <Button
-            onClick={() => handleTypeChange("login")}
-            ariaLabel="Login Tab"
-            className={`p-2 w-full font-semibold transition-all text-2xl ${
-              type === "login"
-                ? "bg-gradient-to-t from-white to-whiteFont-500 dark:from-customBgDark-600 dark:to-customBgDark-500 border-white dark:border-customBgDark-600 dark:hover:border-customBgDark-600 text-black dark:text-white z-10 cursor-default"
-                : "bg-transparent hover:rounded-s-xl text-whiteFont-500 hover:bg-purple-500 hover:text-white dark:bg-transparent dark:text-whiteFont-500 dark:hover:bg-color3-500"
-            } rounded-none rounded-t-xl`}
-          >
-            Login
-          </Button>
-          <Button
-            buttonType="violet"
-            ariaLabel="Register Tab"
-            onClick={() => handleTypeChange("register")}
-            className={`p-2 w-full font-semibold transition-all text-2xl ${
-              type === "register"
-                ? "bg-gradient-to-t from-white to-whiteFont-500 dark:from-customBgDark-600 dark:to-customBgDark-500 border-white dark:border-customBgDark-600 dark:hover:border-customBgDark-600 text-black dark:text-white z-10  cursor-default"
-                : "bg-transparent hover:rounded-e-xl text-whiteFont-500 hover:bg-purple-500 hover:text-white dark:bg-transparent dark:text-whiteFont-500 dark:hover:bg-color3-500"
-            } rounded-none rounded-t-xl `}
-          >
-            Register
-          </Button>
-        </div>
-
         {/* Forms */}
         {type === "login" ? <LoginForm /> : <RegisterForm />}
 
-        {/* Home Link */}
-        <Link
-          to="/"
-          className="mt-6 mx-auto text-gray-700 hover:text-purple-600 dark:text-gray-300 dark:hover:text-white transition flex items-center justify-center"
-        >
-          <FontAwesomeIcon icon={faArrowAltCircleLeft} className="mr-2" />
-          Back to Homepage
-        </Link>
+        {/* Register user link */}
+        <div className="bg-white dark:bg-customBgDark-600 rounded-b-xl w-full px-8 pt-1 pb-8  mx-auto text-gray-700 dark:text-gray-300 hover:text-purple-600 dark:hover:text-white transition flex items-center justify-start">
+          {type === "login" ? (
+            <Link
+              to="/auth?type=register"
+              className="text-gray-500 group dark:text-whiteFont-600 transition mt-2"
+            >
+              New around here?{" "}
+              <span className="text-black dark:text-white  group-hover:text-purple-600 group-dark:hover:text-color3-500 transition">
+                Register here.
+              </span>
+            </Link>
+          ) : (
+            <Link
+              to="/auth?type=login"
+              className="text-gray-500 group dark:text-whiteFont-600 transition mt-2"
+            >
+              Already an user?{" "}
+              <span className="text-black dark:text-white  group-hover:text-purple-600 group-dark:hover:text-color3-500 transition">
+                Login here.
+              </span>
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
