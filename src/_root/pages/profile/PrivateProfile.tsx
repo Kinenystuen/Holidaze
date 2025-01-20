2c7b093948a420146161403b8dc524b4e665eb8e
Kinenystuen <Kine_on1@hotmail.com>
2025-01-20 13:15:09 +0100

    Profile card fix

--- src/_root/pages/profile/PrivateProfile.tsx
+++ src/_root/pages/profile/PrivateProfile.tsx
@@ -21,40 +21,48 @@ const PrivateProfile = () => {
   return (
     <div className="flex flex-col mt-0">
       <div>
-        {/* <div className="h-[50vh] max-h-80 bg-gray-200 dark:bg-gray-800 rounded-xl overflow-hidden">
-          <img
-            src={user.bannerUrl}
-            alt={`${user.name} profile banner`}
-            className="object-cover w-full h-full"
-          />
-        </div> */}
-
         <div className="grid grid-cols-3 grid-rows-1 gap-5 mx-5 mb-20 items-center">
           {/* Left Section (Takes up 2/3 of the row) */}
-          <div className="col-span-3 lg:col-span-2 flex flex-col sm:flex-row h-fit bg-white dark:bg-customBgDark-500 shadow-sm rounded-xl w-full items-center">
-            <div className="w-full sm:w-fit p-2 max-w-72 rounded-xl overflow-hidden">
-              <img
-                src={user.avatar?.url}
-                alt="User Avatar"
-                className="w-full h-40 sm:w-60 sm:h-60 rounded-xl overflow-hidden object-cover"
-              />
+          <div className="col-span-3 lg:col-span-2 flex flex-col h-fit bg-white dark:bg-customBgDark-500 shadow-sm rounded-xl w-full items-center">
+            {/* Banner area */}
+            <div className="flex w-full">
+              {/* Banner */}
+              <div className="h-44 sm:h-16 w-full bg-gray-200 dark:bg-gray-800 rounded-t-xl overflow-hidden">
+                <img
+                  src={user.banner?.url}
+                  alt={`${user.name} profile banner`}
+                  className="object-cover w-full h-full"
+                />
+              </div>
             </div>
-            <div className="flex mx-2 flex-col justify-center items-start sm:items-start text-start sm:text-start x-2 mb-4 sm:ml-6">
-              <H1 className="font-heading">{user.name}</H1>
-              <span className="text-sm font-thin">{user.email}</span>
-              <P className="mt-4 max-w-72">{user.bio}</P>
-              {user.venueManager && (
-                <Link
-                  to={"settings"}
-                  className="my-4 group sm:mb-0 px-3 py-1 w-fit flex items-center cursor-pointer bg-color1 dark:bg-color1-600 text-white dark:text-whiteFont-500 text-sm font-semibold rounded-full whitespace-nowrap transition-all duration-300 ease-in-out"
-                >
-                  <FontAwesomeIcon
-                    icon={faEdit}
-                    className="opacity-0 w-[1px] h-4 p-0 m-0 group-hover:w-4 group-hover:opacity-100 transform scale-90 group-hover:scale-100 transition-all duration-300 ease-in-out group-hover:mr-2"
-                  />
-                  Venue Manager
-                </Link>
-              )}
+            {/* Avatar and description area */}
+            <div className="relative flex flex-col sm:flex-row items-center justify-center w-full">
+              {/* Avatar */}
+              <div className=" min-h-[8rem] sm:h-full sm:min-w-60 sm:min-h-40 flex-1 px-6 sm:w-fit p-2 sm:my-4 max-w-72 rounded-xl overflow-hidden">
+                <img
+                  src={user.avatar?.url}
+                  alt="User Avatar"
+                  className="absolute top-[-8rem] sm:top-[-1.5rem] left-1/2 transform -translate-x-1/2 sm:left-6 sm:translate-x-0 w-80 h-60 sm:w-52 sm:h-52 rounded-xl object-cover border-10 border-white dark:border-gray-800"
+                />
+              </div>
+              {/* User Info */}
+              <div className="flex flex-grow flex-col justify-center items-center sm:items-start text-center sm:text-start mb-4 sm:ml-1">
+                <H1 className="font-heading">{user.name}</H1>
+                <span className="text-sm font-thin">{user.email}</span>
+                <P className="mt-4 max-w-72">{user.bio}</P>
+                {user.venueManager && (
+                  <Link
+                    to={"settings"}
+                    className="my-4 group sm:mb-0 px-3 py-1 w-fit flex items-center cursor-pointer bg-color1 dark:bg-color1-600 text-white dark:text-whiteFont-500 text-sm font-semibold rounded-full whitespace-nowrap transition-all duration-300 ease-in-out"
+                  >
+                    <FontAwesomeIcon
+                      icon={faEdit}
+                      className="opacity-0 w-[1px] h-4 p-0 m-0 group-hover:w-4 group-hover:opacity-100 transform scale-90 group-hover:scale-100 transition-all duration-300 ease-in-out group-hover:mr-2"
+                    />
+                    Venue Manager
+                  </Link>
+                )}
+              </div>
             </div>
           </div>
 
