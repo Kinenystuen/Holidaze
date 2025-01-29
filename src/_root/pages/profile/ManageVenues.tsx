a23404283c3069b2ac796f2a488711dbc452f154
Kinenystuen <Kine_on1@hotmail.com>
2025-01-29 09:51:16 +0100

    Enhance ManageVenues component to include a title and integrate MyVenues for better user experience

--- src/_root/pages/profile/ManageVenues.tsx
+++ src/_root/pages/profile/ManageVenues.tsx
@@ -1,5 +1,15 @@
+import H1 from "../../../components/shared/Typography/H1";
+import MyVenues from "./MyVenues";
+
 const ManageVenues = () => {
-  return <div>ManageVenues</div>;
+  return (
+    <div>
+      <H1 className="max-w-screen-lg mx-auto font-semibold px-10 mt-4">
+        My Venues
+      </H1>
+      <MyVenues />
+    </div>
+  );
 };
 
 export default ManageVenues;
