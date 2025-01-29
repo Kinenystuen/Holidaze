bcacab74647c9bd83ac2abb8572f7ce14ae2aa0c
Kinenystuen <Kine_on1@hotmail.com>
2025-01-29 09:50:57 +0100

    Refactor AdminCreateVenue component to use shared Typography components for headings and paragraphs

--- src/_root/pages/profile/AdminCreateVenue.tsx
+++ src/_root/pages/profile/AdminCreateVenue.tsx
@@ -1,13 +1,15 @@
 import { VenueProvider } from "../../../components/context/VenueContext";
 import VenueForm from "../../../components/createVenue/CreateVenueForm";
+import H2 from "../../../components/shared/Typography/H2";
+import P from "../../../components/shared/Typography/P";
 
 const AdminCreateVenue = () => {
   return (
     <VenueProvider>
       <div className="container mx-auto mb-20">
-        <div className="max-w-screen-xl mx-auto px-10">
-          <h2 className="text-2xl font-semibold mt-8">Create Venue</h2>
-          <p className="mt-4">Create a new venue to add to the database.</p>
+        <div className=" max-w-2xl mx-auto px-10">
+          <H2 className="text-2xl font-semibold mt-8">Create Venue</H2>
+          <P className="mt-4">Create a new venue to start hosting</P>
           <VenueForm />
         </div>
       </div>
