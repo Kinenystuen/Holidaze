49e0ee588fe189315f4bcbdb71c3c23862e801e9
Kinenystuen <Kine_on1@hotmail.com>
2025-01-29 13:11:37 +0100

    Add H1 component to AdminBookings for improved title styling

--- src/_root/pages/profile/AdminBookings.tsx
+++ src/_root/pages/profile/AdminBookings.tsx
@@ -1,5 +1,14 @@
+import H1 from "../../../components/shared/Typography/H1";
+import MyBookings from "./MyBookings";
+
 const AdminBookings = () => {
-  return <div>AdminBookings</div>;
+  return (
+    <div>
+      <H1 className="max-w-screen-lg mx-auto font-semibold px-10 mt-4">
+        Bookings at my venues
+      </H1>
+    </div>
+  );
 };
 
 export default AdminBookings;
