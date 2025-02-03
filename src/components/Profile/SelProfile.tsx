597b3d0b09d944adfdd688f9c9898fc535c07b39
Kinenystuen <Kine_on1@hotmail.com>
2025-02-03 21:25:24 +0100

    Refactor SelProfile component to replace VenuesData with UserVenues and enhance user name display

--- src/components/Profile/SelProfile.tsx
+++ src/components/Profile/SelProfile.tsx
@@ -2,6 +2,7 @@ import { UserProfile } from "../library/types";
 import VenuesData from "../../_root/pages/venues/VenuesData";
 import H2 from "../shared/Typography/H2";
 import ProfileCard from "../ui/ProfileCard";
+import UserVenues from "./UserVenues";
 
 interface SelUserProps {
   user: UserProfile;
@@ -19,7 +20,7 @@ const SelProfile: React.FC<SelUserProps> = ({
         {/* Open box */}
         <div className="col-span-4 md:col-span-3 lg:col-span-1 flex whitespace-nowrap items-center justify-center"></div>
       </div>
-      <div className="max-w-3xl flex flex-col flex-wrap gap-5 mt-10 my-20 mx-5">
+      <div className=" flex flex-col flex-wrap gap-5 mt-10 my-20 mx-5">
         <div className="flex items-center gap-4">
           <div className="rounded-full overflow-hidden w-8 h-8 flex items-center justify-center bg-gray-200 dark:bg-gray-800 shadow-md text-">
             <img
@@ -28,16 +29,16 @@ const SelProfile: React.FC<SelUserProps> = ({
               alt={user.avatar.alt}
             />
           </div>
-          <H2 className="md:text-lg font-semibold capitalize">
+          <H2 className="md:text-2xl font-semibold capitalize">
             {user.name}'s venues ({user.venues.length}):
           </H2>
         </div>
         {user.venues.length > 0 ? (
-          <VenuesData venues={user.venues} />
+          <UserVenues venues={user.venues} />
         ) : (
           <div className="flex items-center justify-center p-4 bg-gray-200 dark:bg-gray-800 rounded-lg shadow-md">
             <H2 className="text-lg font-semibold capitalize">
-              {user.name} has no venues yet.
+              {user.name} has no venues.
             </H2>
           </div>
         )}
