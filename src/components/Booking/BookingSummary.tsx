942348379ffd1b60d8a87d9eaa54edca8e4d7cf9
Kinenystuen <Kine_on1@hotmail.com>
2025-02-06 10:50:36 +0100

    Calculate total price and update nights in BookingSummary component

--- src/components/Booking/BookingSummary.tsx
+++ src/components/Booking/BookingSummary.tsx
@@ -16,38 +16,44 @@ const BookingSummary: React.FC<BookingSummaryProps> = ({
   guests
 }) => {
   const [nights, setNights] = useState<number>(0);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+
+  /* Function to calculate nights */
   const calculateNights = (dateFrom: Date, dateTo: Date): number => {
     if (!(dateFrom instanceof Date) || !(dateTo instanceof Date)) {
       throw new Error("Invalid date format");
     }
+
     const startDate = new Date(dateFrom.setHours(0, 0, 0, 0));
     const endDate = new Date(dateTo.setHours(0, 0, 0, 0));
 
-    if (endDate <= startDate) return 0;
+    if (endDate <= startDate) return 1;
     return Math.floor(
       (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
     );
   };
-  useEffect(
-    () => setNights(calculateNights(dateFrom, dateTo)),
-    [dateFrom, dateTo]
-  );
+
+  /* Update nights & total price when dates or guests change */
+  useEffect(() => {
+    const calculatedNights = calculateNights(dateFrom, dateTo);
+    setNights(calculatedNights);
+    setTotalPrice(price * calculatedNights * guests);
+  }, [dateFrom, dateTo, guests, price]);
 
   return (
-    <div className="flex-1 p-4 w-full ">
+    <div className="flex-1 p-4 w-full">
       <H3 className="text-lg font-semibold">Booking Summary</H3>
       <P className="flex justify-between">
         <span>Guests:</span> {guests}
       </P>
       <P className="flex justify-between">
-        <span>Nights:</span> {calculateNights(dateFrom, dateTo)}
+        <span>Nights:</span> {nights}
       </P>
-      <H3 className="flex text-md justify-between mt-3">Total Price</H3>
-      <P className="flex justify-between">
-        <span>
-          {price * guests} kr x {nights} {nights > 1 ? "nights" : "night"}:
-        </span>
-        {price * nights} kr
+      <H3 className="flex text-md justify-between mt-3 border-t border-color2-600 pt-2">
+        Total Price
+      </H3>
+      <P className="flex text-3xl text-whiteFont-100 justify-end">
+        {totalPrice} kr
       </P>
     </div>
   );
