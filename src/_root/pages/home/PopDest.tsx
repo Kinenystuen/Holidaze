848aa3a44d4242da0369c26ae9ccb020d3775a4d
Kinenystuen <Kine_on1@hotmail.com>
2025-02-13 10:13:20 +0100

    Refactor PopDest component: rename section title to 'Guests' Favorites' for improved clarity

--- src/_root/pages/home/PopDest.tsx
+++ src/_root/pages/home/PopDest.tsx
@@ -7,7 +7,8 @@ const PopDest = () => {
   return (
     <div className="container max-w-screen-xl mx-auto my-20 ">
       <div className="relative">
-        <H2 className="my-6 text-2xl px-10">Popular Destinations</H2>
+        <H2 className="my-6 text-2xl px-10">Guests' Favorites</H2>
+
         <TopRated />
         <div className="flex justify-center mt-5">
           <Link to="/venues">
